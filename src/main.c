563b7866251bae0a8f06e432cc3ef2e38e8d3c6d
toxypiks <HannaHinkelbein89@gmx.de>
2025-05-15 14:45:34 +0200

    Implement swap

--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+
+void swap(int *x, int *y)
+{
+    int tmp = *x; // value from x into tmp
+    *x = *y;
+    *y = tmp;
+}
+
 int main (void)
 {
-    printf("Hello World!\n");
+    int x = 4;
+    int y = 10;
+    swap(&x, &y);
+    printf("x: %d y: %d\n", x, y);
     return 0;
 }
