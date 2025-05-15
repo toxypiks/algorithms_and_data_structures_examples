e3058760258ed571db3c23994f6efb1aafc2652a
toxypiks <HannaHinkelbein89@gmx.de>
2025-05-15 15:26:40 +0200

    Implement macro to find array length

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#define ARR_LEN(xs) sizeof(xs)/sizeof(xs[0])
 
 void swap(int *x, int *y)
 {
@@ -8,11 +9,22 @@ void swap(int *x, int *y)
     *y = tmp;
 }
 
+void quicksort(float **data, size_t *left, size_t *right)
+{
+    if (*right <= *left) {
+        return;
+    }
+    // WIP
+}
+
 int main (void)
 {
     int x = 4;
     int y = 10;
     swap(&x, &y);
     printf("x: %d y: %d\n", x, y);
+
+    float my_array[7] = {3, 7, 4, 8, 23, 45, 2};
+    printf("%d\n", ARR_LEN(my_array));
     return 0;
 }
