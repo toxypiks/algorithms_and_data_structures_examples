e1e0ebbc1c6218959fbda2dd101b00a0fc048b03
toxypiks <HannaHinkelbein89@gmx.de>
2025-05-24 15:26:07 +0200

    Implement heap sort

--- src/heap_sort.c
+++ src/heap_sort.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define ARRAY_LEN(xs) sizeof(xs)/sizeof(xs[0])
+
 void heapify(int *data, int n, int i)
 {
     // n = array_len, i = index array
@@ -33,7 +35,39 @@ void heapify(int *data, int n, int i)
 
 }
 
+void heap_sort(int *data, int n)
+{
+    // rearrange initial array into max heap
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(data, n , i);
+    }
+
+    // put biggest elements to the left one by one
+    for (int i = n - 1; i > 0; i--) {
+
+        // move current root to end of array
+        int tmp = data[0];
+        data[0] = data[i];
+        data[i] = tmp;
+
+        // call max heapify on remaining unordered array
+        heapify(data, i, 0);
+    }
+}
+
+void print_data(int *data, int n)
+{
+    for (int i = 0; i < n; i++) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+}
+
 int main(void)
 {
+    int data[] = {9, 4, 3, 8, 10, 2, 5};
+
+    heap_sort(data, ARRAY_LEN(data));
+    print_data(data, ARRAY_LEN(data));
     return 0;
 }
