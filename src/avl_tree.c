d8c09a5667668744cc5543b358ec44adef31592b
toxypiks <HannaHinkelbein89@gmx.de>
2025-05-25 11:46:54 +0200

    Implement function to delete node

--- src/avl_tree.c
+++ src/avl_tree.c
@@ -110,6 +110,81 @@ AVLNode* avl_insert(AVLNode *root, int key)
     return root;
 }
 
+AVLNode* find_min_value_node(AVLNode* node)
+{
+    AVLNode* current = node;
+
+    while(current->left != NULL) {
+        current = current->left;
+    }
+    return current;
+}
+
+AVLNode* delete_node(AVLNode* root, int key)
+{
+    if (root == NULL) {
+        return root;
+    }
+
+    if (key < root->key) {
+        root->left = delete_node(root->left, key);
+    }
+    else if (key > root->key) {
+        root->right = delete_node (root->right, key);
+    }
+    else {
+        // key same as root -> root needs to be deleted
+        if (root->left == NULL || root->right == NULL) {
+            AVLNode* tmp = root->left? root->left : root->right;
+
+            if (tmp == NULL)
+            {
+                tmp = root;
+                root = NULL;
+            }
+            else {
+                // one child
+                *root = *tmp; // value of child becomes root value
+                free(tmp);
+            }
+        }
+        else { // node with two children
+            AVLNode* tmp = find_min_value_node(root->right);
+            root->key = tmp->key;
+            root->right = delete_node(root->right, tmp->key);
+        }
+    }
+
+    if (root == NULL) {
+        return root;
+    }
+
+    root->height = 1 + max(get_height(root->left), get_height(root->right));
+    int balance = get_balance(root);
+
+    // 4 unbalanced cases
+
+    if (balance > 1 && get_balance(root->left) >= 0) {
+        return right_rotate(root);
+    }
+
+    if (balance > 1 && get_balance(root->left) < 0) {
+        root->left = left_rotate(root->left);
+        return right_rotate(root);
+    }
+
+    if (balance < -1 && get_balance(root->right) <= 0) {
+        return left_rotate(root);
+    }
+
+    if (balance < -1 && get_balance(root->right) > 0) {
+        root->right = right_rotate(root->right);
+        return left_rotate(root);
+    }
+
+    return root;
+}
+
 void print_preorder(AVLNode *root)
 {
     if (root != NULL)
